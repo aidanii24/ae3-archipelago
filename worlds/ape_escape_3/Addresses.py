276f6e5254f3fa24700ac405ac09cb69ef3bed85
aidanii <aidankuya24@gmail.com>
2025-02-19 22:45:59 +0000

    Uploaded from Local Repository

---
+++ worlds/ape_escape_3/Addresses.py
@@ -0,0 +1,29 @@
+class Address:
+    items = {
+        # <!> Values are on/off between 0x01 and 0x02 respectively unless commented otherwise.
+
+        # Gadgets
+        "stun_club" : 0x649950,
+        "monkey_net" : 0x649954,
+        "monkey_radar" : 0x649958,
+        "super_hoop" : 0x649960,
+        "rc_car" : 0x649964,
+        "sky_flyer" : 0x649968,
+        "water_net" : 0x649978,
+
+        # Morphs
+        "knight" : 0x649930,
+        "cowboy" : 0x649934,
+        "ninja" : 0x649938,
+        "magician" : 0x64993c,
+        "kungfu" : 0x649940,
+        "hero" : 0x649944,
+        "monkey" : 0x649948,
+
+        # Accessories
+        "morph_stock" : 0x649928,   # Ranged Value
+
+        "chassis_twin" : 0x649c98,
+        "chassis_black" : 0x649c99,
+        "chassis_pudding" : 0x649c9a,
+    }
