ce5b24c7129a5af96a5316992510a71ea71d35e9
aidanii <aidanii_real@proton.me>
2025-03-03 10:46:08 +0800

    - Minor Refactor of Logic.py

--- worlds/ape_escape_3/data/Logic.py
+++ worlds/ape_escape_3/data/Logic.py
@@ -2,46 +2,52 @@ from typing import Set, Callable
 
 from BaseClasses import CollectionState
 
-from .Strings import AE3Items
+from .Strings import Itm, APHelper
 
 # General Checks
 ## Check if Player can Catch Monkeys
 def can_catch(state : CollectionState, player : int):
-    return state.has(AE3Items.monkey_net.value, player)
+    return can_net(state, player) or can_morph_not_monkey(state, player)
+
+def can_net(state : CollectionState, player : int):
+    return state.has(Itm.gadget_net.value, player)
 
 ## Check if Player can Morph (Any Morph)
 def can_morph(state : CollectionState, player : int):
-    return state.has("Morphs", player)
+    return state.has(APHelper.morphs.value, player)
+
+def can_morph_not_monkey(state : CollectionState, player : int):
+    return state.has(APHelper.morphs_no_monkey.value, player)
 
 ## Check if Player can use long-ranged attacks
 def can_shoot(state : CollectionState, player : int):
-    return (state.has(AE3Items.slingback_shooter.value, player) or
+    return (state.has(Itm.gadget_sling.value, player) or
 
-            state.has(AE3Items.morph_cowboy.value, player) or
-            state.has(AE3Items.morph_hero.value, player))
+            state.has(Itm.morph_cowboy.value, player) or
+            state.has(Itm.morph_hero.value, player))
 
 ## Check if Player can fly/glide
 def can_fly(state : CollectionState, player : int):
-    return (state.has(AE3Items.sky_flyer.value, player) or
+    return (state.has(Itm.gadget_fly.value, player) or
 
-            state.has(AE3Items.morph_ninja.value, player) or
-            state.has(AE3Items.morph_hero.value, player))
+            state.has(Itm.morph_ninja.value, player) or
+            state.has(Itm.morph_hero.value, player))
 
 # Gadget Checks
-## Check if Player can use long-ranged attacks freely and can aim it manually (e.g. has the Slingback Shooter)
-def can_shoot_free(state : CollectionState, player : int):
-    return state.has(AE3Items.slingback_shooter.value, player)
+## Check if Player can use the Slingback Shooter
+def can_sling(state : CollectionState, player : int):
+    return state.has(Itm.gadget_sling.value, player)
 
 ## Check if Player can use the RC Car
-def can_use_rc_car(state : CollectionState, player : int):
-    return state.has(AE3Items.rc_car.value, player)
+def can_rcc(state : CollectionState, player : int):
+    return state.has(Itm.gadget_rcc.value, player)
 
 # Morph Checks
-def can_use_genie(state : CollectionState, player : int):
-    return state.has(AE3Items.morph_magician.value, player)
+def can_genie(state : CollectionState, player : int):
+    return state.has(Itm.morph_magician.value, player)
 
-def can_use_monkey(state : CollectionState, player : int):
-    return state.has(AE3Items.morph_monkey.value, player)
+def can_monkey(state : CollectionState, player : int):
+    return state.has(Itm.morph_monkey.value, player)
 
 class AccessRules:
     """
@@ -49,24 +55,25 @@ class AccessRules:
     """
 
     # General
-    CATCH = can_catch               # Net Unlocked (if Shuffled) or has any morph (If Gadget Association is disabled)
+    CATCH = can_catch                           # Monkey Net unlocked or any Morph that can Catch Monkeys
     MORPH = can_morph
-    SHOOT = can_shoot               # Slingback Shooter unlocked or has any morph with long range attacks
-    FLY = can_fly                   # Sky Flyer unlocked or has any morph that can fly/glide
+    MORPH_NO_MONKEY = can_morph_not_monkey      # Unlocked any morph that is not Super Monkey
+    SHOOT = can_shoot                           # Slingback Shooter unlocked or has any morph with long range attacks
+    FLY = can_fly                               # Sky Flyer unlocked or has any morph that can fly/glide
 
     # Gadget
-    SHOOT_FREE = can_shoot_free     # Slingback Shooter specifically unlocked
-    RC_CAR = can_use_rc_car
+    SLING = can_sling
+    RCC = can_rcc
 
     # Morph
-    GENIE = can_use_genie
-    MONKEY = can_use_monkey
+    GENIE = can_genie
+    MONKEY = can_monkey
 
     # Glitches
     GLITCH_FLY = 0
     GLITCH_FLOAT = 0
 
-class LocationRules:
+class Rulesets:
     """
     Helper for Storing and Managing Access Rules of Locations.
 
