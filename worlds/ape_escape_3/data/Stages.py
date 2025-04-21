d66464287e03d9bf6cd6211d685ad15b17b0ac62
aidanii <aidanii_real@proton.me>
2025-04-21 22:18:12 +0800

    - Updated Stages.py to account for Region Overhaul

--- worlds/ape_escape_3/data/Stages.py
+++ worlds/ape_escape_3/data/Stages.py
@@ -52,13 +52,13 @@ LEVELS_BY_ORDER : Sequence[str] = [
 
     Stage.region_onsen_a.value,
     Stage.region_snowfesta_a.value,
-    Stage.region_edotown_a.value,
+    Stage.region_edotown_a1.value,
 
     Stage.region_boss3.value,
 
     Stage.region_heaven_a1.value,
     Stage.region_toyhouse_a.value,
-    Stage.region_iceland_a.value,
+    Stage.region_iceland_a1.value,
     Stage.region_arabian_a.value,
 
     Stage.region_boss4.value,
@@ -85,7 +85,8 @@ STAGES_TITLE : Sequence[str] = [
 ]
 
 STAGES_HUB : Sequence[str] = [
-    Stage.travel_station_a.value, Stage.travel_station_b.value
+    Stage.travel_station_a.value,
+    Stage.travel_station_b.value
 ]
 
 STAGES_ZERO : Sequence[str] = [
@@ -93,118 +94,287 @@ STAGES_ZERO : Sequence[str] = [
 ]
 
 STAGES_SEASIDE : Sequence[str] = [
-    Stage.region_seaside_a.value, Stage.region_seaside_b.value, Stage.region_seaside_c.value
+    Stage.region_seaside_a.value,
+    Stage.region_seaside_b.value,
+    Stage.region_seaside_c.value
 ]
 
 STAGES_WOODS : Sequence[str] = [
-    Stage.region_woods_a.value, Stage.region_woods_b.value, Stage.region_woods_c.value, Stage.region_woods_d.value
+    Stage.region_woods_a.value,
+    Stage.region_woods_b.value,
+    Stage.region_woods_c.value,
+    Stage.region_woods_d.value
 ]
 
 STAGES_CASTLE : Sequence[str] = [
-    Stage.region_castle_a.value, Stage.region_castle_b.value, Stage.region_castle_c.value, Stage.region_castle_d.value, Stage.region_castle_e.value,
+    Stage.region_castle_a1.value,
+    Stage.region_castle_a.value,
+    Stage.region_castle_b.value,
+    Stage.region_castle_b1.value,
+    Stage.region_castle_c.value,
+    Stage.region_castle_d.value,
+    Stage.region_castle_d1.value,
+    Stage.region_castle_e.value,
     Stage.region_castle_f.value
 ]
 
 STAGES_CISCOCITY : Sequence[str] = [
-    Stage.region_ciscocity_a.value, Stage.region_ciscocity_b.value, Stage.region_ciscocity_c.value, Stage.region_ciscocity_d.value,
+    Stage.region_ciscocity_a.value,
+    Stage.region_ciscocity_b.value,
+    Stage.region_ciscocity_c.value,
+    Stage.region_ciscocity_c1.value,
+    Stage.region_ciscocity_d.value,
     Stage.region_ciscocity_e.value
 ]
 
 STAGES_STUDIO : Sequence[str] = [
-    Stage.region_studio_a.value, Stage.region_studio_a1.value, Stage.region_studio_b.value, Stage.region_studio_c.value,
-    Stage.region_studio_d.value, Stage.region_studio_e.value, Stage.region_studio_f.value, Stage.region_studio_g.value
+    Stage.region_studio_a.value,
+    Stage.region_studio_a1.value,
+    Stage.region_studio_a2.value,
+    Stage.region_studio_b.value,
+    Stage.region_studio_b1.value,
+    Stage.region_studio_c.value,
+    Stage.region_studio_d.value,
+    Stage.region_studio_d1.value,
+    Stage.region_studio_d2.value,
+    Stage.region_studio_e.value,
+    Stage.region_studio_f.value,
+    Stage.region_studio_f1.value,
+    Stage.region_studio_g.value
 ]
 
 STAGES_HALLOWEEN : Sequence[str] = [
-    Stage.region_halloween_a1.value, Stage.region_halloween_a.value, Stage.region_halloween_b.value, Stage.region_halloween_c.value,
-    Stage.region_halloween_c1.value, Stage.region_halloween_d.value, Stage.region_halloween_e.value, Stage.region_halloween_e.value,
+    Stage.region_halloween_a.value,
+    Stage.region_halloween_a1.value,
+    Stage.region_halloween_b.value,
+    Stage.region_halloween_b1.value,
+    Stage.region_halloween_c.value,
+    Stage.region_halloween_c1.value,
+    Stage.region_halloween_c2.value,
+    Stage.region_halloween_d.value,
+    Stage.region_halloween_d1.value,
+    Stage.region_halloween_d2.value,
+    Stage.region_halloween_e.value,
     Stage.region_halloween_f.value
 ]
 
 STAGES_WESTERN : Sequence[str] = [
-    Stage.region_western_a.value, Stage.region_western_b.value, Stage.region_western_c.value, Stage.region_western_d.value, Stage.region_western_e.value,
+    Stage.region_western_a.value,
+    Stage.region_western_b.value,
+    Stage.region_western_b1.value,
+    Stage.region_western_c.value,
+    Stage.region_western_d.value,
+    Stage.region_western_d1.value,
+    Stage.region_western_d2.value,
+    Stage.region_western_d3.value,
+    Stage.region_western_e.value,
+    Stage.region_western_e1.value,
     Stage.region_western_f.value
 ]
 
 STAGES_ONSEN : Sequence[str] = [
-    Stage.region_onsen_a.value, Stage.region_onsen_a1.value, Stage.region_onsen_a2.value, Stage.region_onsen_b.value, Stage.region_onsen_b1.value,
-    Stage.region_onsen_b.value, Stage.region_onsen_b2.value, Stage.region_onsen_c.value, Stage.region_onsen_d.value, Stage.region_onsen_d1.value,
+    Stage.region_onsen_a.value,
+    Stage.region_onsen_a1.value,
+    Stage.region_onsen_a2.value,
+    Stage.region_onsen_b.value,
+    Stage.region_onsen_b1.value,
+    Stage.region_onsen_c.value,
+    Stage.region_onsen_d.value,
+    Stage.region_onsen_d1.value,
     Stage.region_onsen_e.value
 ]
 
 STAGES_SNOWFESTA : Sequence[str] = [
-    Stage.region_snowfesta_a.value, Stage.region_snowfesta_b.value, Stage.region_snowfesta_c.value, Stage.region_snowfesta_d.value,
-    Stage.region_snowfesta_e.value, Stage.region_snowfesta_f.value, Stage.region_snowfesta_g.value
+    Stage.region_snowfesta_a.value,
+    Stage.region_snowfesta_b.value,
+    Stage.region_snowfesta_c.value,
+    Stage.region_snowfesta_d.value,
+    Stage.region_snowfesta_e.value,
+    Stage.region_snowfesta_f.value,
+    Stage.region_snowfesta_g.value
 ]
 
 STAGES_EDOTOWN : Sequence[str] = [
-    Stage.region_edotown_a.value, Stage.region_edotown_b1.value, Stage.region_edotown_b.value, Stage.region_edotown_c1.value,
-    Stage.region_edotown_c.value, Stage.region_edotown_d.value, Stage.region_edotown_e.value, Stage.region_edotown_f.value
+    Stage.region_edotown_a.value,
+    Stage.region_edotown_a1.value,
+    Stage.region_edotown_b.value,
+    Stage.region_edotown_b1.value,
+    Stage.region_edotown_c.value,
+    Stage.region_edotown_c1.value,
+    Stage.region_edotown_c2.value,
+    Stage.region_edotown_d.value,
+    Stage.region_edotown_e.value,
+    Stage.region_edotown_f.value
 ]
 
 STAGES_HEAVEN : Sequence[str] = [
-    Stage.region_heaven_a.value, Stage.region_heaven_a1.value, Stage.region_heaven_a2.value, Stage.region_heaven_b.value, Stage.region_heaven_c.value,
-    Stage.region_heaven_d.value, Stage.region_heaven_e.value
+    Stage.region_heaven_a.value,
+    Stage.region_heaven_a1.value,
+    Stage.region_heaven_b.value,
+    Stage.region_heaven_c.value,
+    Stage.region_heaven_d.value,
+    Stage.region_heaven_e.value
 ]
 
 STAGES_TOYHOUSE : Sequence[str] = [
-    Stage.region_toyhouse_a.value, Stage.region_toyhouse_b.value, Stage.region_toyhouse_c.value, Stage.region_toyhouse_d.value,
-    Stage.region_toyhouse_e.value, Stage.region_toyhouse_e1.value, Stage.region_toyhouse_f.value, Stage.region_toyhouse_g.value,
+    Stage.region_toyhouse_a.value,
+    Stage.region_toyhouse_b.value,
+    Stage.region_toyhouse_b1.value,
+    Stage.region_toyhouse_c.value,
+    Stage.region_toyhouse_d.value,
+    Stage.region_toyhouse_e.value,
+    Stage.region_toyhouse_f.value,
+    Stage.region_toyhouse_g.value,
+    Stage.region_toyhouse_g1.value,
     Stage.region_toyhouse_h.value
 ]
 
 STAGES_ICELAND : Sequence[str] = [
-    Stage.region_iceland_a.value, Stage.region_iceland_b.value, Stage.region_iceland_c.value, Stage.region_iceland_d.value, Stage.region_iceland_e.value,
+    Stage.region_iceland_a.value,
+    Stage.region_iceland_a1.value,
+    Stage.region_iceland_a2.value,
+    Stage.region_iceland_b.value,
+    Stage.region_iceland_c.value,
+    Stage.region_iceland_d.value,
+    Stage.region_iceland_e.value,
     Stage.region_iceland_f.value
 ]
 
 STAGES_ARABIAN : Sequence[str] = [
-    Stage.region_arabian_a.value, Stage.region_arabian_b.value, Stage.region_arabian_c.value, Stage.region_arabian_c1.value, Stage.region_arabian_e.value,
-    Stage.region_arabian_e1.value, Stage.region_arabian_f.value, Stage.region_arabian_g.value
+    Stage.region_arabian_a.value,
+    Stage.region_arabian_b.value,
+    Stage.region_arabian_c1.value,
+    Stage.region_arabian_c.value,
+    Stage.region_arabian_e.value,
+    Stage.region_arabian_e1.value,
+    Stage.region_arabian_f.value,
+    Stage.region_arabian_g.value
 ]
 
 STAGES_ASIA : Sequence[str] = [
-    Stage.region_asia_a.value, Stage.region_asia_a1.value, Stage.region_asia_a2.value, Stage.region_asia_a3.value, Stage.region_asia_a4.value,
-    Stage.region_asia_a5.value, Stage.region_asia_b.value, Stage.region_asia_b1.value, Stage.region_asia_b2.value, Stage.region_asia_d.value,
-    Stage.region_asia_d1.value, Stage.region_asia_d2.value, Stage.region_asia_e.value, Stage.region_asia_e1.value, Stage.region_asia_e2.value,
+    Stage.region_asia_a.value,
+    Stage.region_asia_a1.value,
+    Stage.region_asia_a2.value,
+    Stage.region_asia_a3.value,
+    Stage.region_asia_a4.value,
+    Stage.region_asia_a5.value,
+    Stage.region_asia_a6.value,
+    Stage.region_asia_b.value,
+    Stage.region_asia_b1.value,
+    Stage.region_asia_b2.value,
+    Stage.region_asia_d.value,
+    Stage.region_asia_d1.value,
+    Stage.region_asia_d2.value,
+    Stage.region_asia_e.value,
+    Stage.region_asia_e1.value,
+    Stage.region_asia_e2.value,
     Stage.region_asia_f.value
 ]
 
 STAGES_PLANE : Sequence[str] = [
-    Stage.region_plane_a.value, Stage.region_plane_a1.value, Stage.region_plane_b.value, Stage.region_plane_b1.value, Stage.region_plane_c.value,
-    Stage.region_plane_c1.value, Stage.region_plane_d.value, Stage.region_plane_e.value, Stage.region_plane_f.value, Stage.region_plane_f1.value,
-    Stage.region_plane_g.value, Stage.region_plane_h.value
+    Stage.region_plane_a.value,
+    Stage.region_plane_a1.value,
+    Stage.region_plane_b.value,
+    Stage.region_plane_b1.value,
+    Stage.region_plane_b2.value,
+    Stage.region_plane_c.value,
+    Stage.region_plane_c1.value,
+    Stage.region_plane_d.value,
+    Stage.region_plane_e.value,
+    Stage.region_plane_f.value,
+    Stage.region_plane_f1.value,
+    Stage.region_plane_g.value,
+    Stage.region_plane_h.value
 ]
 
 STAGES_HONG : Sequence[str] = [
-    Stage.region_hong_a.value, Stage.region_hong_a1.value, Stage.region_hong_a2.value, Stage.region_hong_b.value, Stage.region_hong_b1.value,
-    Stage.region_hong_c.value, Stage.region_hong_c1.value, Stage.region_hong_c2.value, Stage.region_hong_d.value, Stage.region_hong_e.value,
-    Stage.region_hong_e1.value, Stage.region_hong_f.value, Stage.region_hong_g.value, Stage.region_hong_h.value
+    Stage.region_hong_a.value,
+    Stage.region_hong_a1.value,
+    Stage.region_hong_a2.value,
+    Stage.region_hong_b.value,
+    Stage.region_hong_b1.value,
+    Stage.region_hong_b2.value,
+    Stage.region_hong_c.value,
+    Stage.region_hong_c1.value,
+    Stage.region_hong_c2.value,
+    Stage.region_hong_d.value,
+    Stage.region_hong_e.value,
+    Stage.region_hong_e1.value,
+    Stage.region_hong_f.value,
+    Stage.region_hong_g.value,
+    Stage.region_hong_h.value
 ]
 
 STAGES_BAY : Sequence[str] = [
-    Stage.region_bay_a.value, Stage.region_bay_a1.value, Stage.region_bay_a2.value, Stage.region_bay_a3.value, Stage.region_bay_a4.value,
-    Stage.region_bay_a5.value, Stage.region_bay_b.value, Stage.region_bay_c.value, Stage.region_bay_d.value, Stage.region_bay_d1.value,
-    Stage.region_bay_e.value, Stage.region_bay_e1.value, Stage.region_bay_e2.value, Stage.region_bay_e3.value, Stage.region_bay_f.value
+    Stage.region_bay_a.value,
+    Stage.region_bay_a1.value,
+    Stage.region_bay_a2.value,
+    Stage.region_bay_a3.value,
+    Stage.region_bay_a4.value,
+    Stage.region_bay_a5.value,
+    Stage.region_bay_a6.value,
+    Stage.region_bay_a7.value,
+    Stage.region_bay_b.value,
+    Stage.region_bay_c.value,
+    Stage.region_bay_c1.value,
+    Stage.region_bay_d.value,
+    Stage.region_bay_d1.value,
+    Stage.region_bay_e.value,
+    Stage.region_bay_e1.value,
+    Stage.region_bay_e2.value,
+    Stage.region_bay_f.value
 ]
 
 STAGES_TOMO : Sequence[str] = [
-    Stage.region_tomo_a.value, Stage.region_tomo_a1.value, Stage.region_tomo_b.value, Stage.region_tomo_c.value, Stage.region_tomo_e.value,
-    Stage.region_tomo_e1.value, Stage.region_tomo_e2.value, Stage.region_tomo_f.value, Stage.region_tomo_f1.value, Stage.region_tomo_f2.value,
-    Stage.region_tomo_g.value, Stage.region_tomo_g1.value, Stage.region_tomo_h.value, Stage.region_tomo_h1.value, Stage.region_tomo_i.value,
-    Stage.region_tomo_j.value,
+    Stage.region_tomo_a.value,
+    Stage.region_tomo_a1.value,
+    Stage.region_tomo_b.value,
+    Stage.region_tomo_c.value,
+    Stage.region_tomo_e.value,
+    Stage.region_tomo_e1.value,
+    Stage.region_tomo_e2.value,
+    Stage.region_tomo_e3.value,
+    Stage.region_tomo_f.value,
+    Stage.region_tomo_f1.value,
+    Stage.region_tomo_f2.value,
+    Stage.region_tomo_g.value,
+    Stage.region_tomo_g1.value,
+    Stage.region_tomo_h.value,
+    Stage.region_tomo_h1.value,
+    Stage.region_tomo_i.value,
+    Stage.region_tomo_j.value
+
+
+
 ]
 
 STAGES_SPACE : Sequence[str] = [
-    Stage.region_space_a.value, Stage.region_space_b.value, Stage.region_space_d.value, Stage.region_space_e.value, Stage.region_space_e1.value,
-    Stage.region_space_f.value, Stage.region_space_f1.value, Stage.region_space_f2.value, Stage.region_space_g.value, Stage.region_space_g1.value,
-    Stage.region_space_g2.value, Stage.region_space_h.value, Stage.region_space_i.value, Stage.region_space_j.value, Stage.region_space_k.value,
-    Stage.region_space_l.value,
+    Stage.region_space_a.value,
+    Stage.region_space_b.value,
+    Stage.region_space_d.value,
+    Stage.region_space_e.value,
+    Stage.region_space_e1.value,
+    Stage.region_space_f.value,
+    Stage.region_space_f1.value,
+    Stage.region_space_f2.value,
+    Stage.region_space_g.value,
+    Stage.region_space_g1.value,
+    Stage.region_space_h.value,
+    Stage.region_space_i.value,
+    Stage.region_space_j.value,
+    Stage.region_space_j1.value,
+    Stage.region_space_k.value
 ]
 
 STAGES_BOSSES : Sequence[str] = [
-    Stage.region_boss1.value, Stage.region_boss2.value, Stage.region_boss3.value, Stage.region_boss4.value, Stage.region_boss5.value, Stage.region_boss6.value,
-    Stage.region_specter1.value, Stage.region_specter2.value
+    Stage.region_boss1.value,
+    Stage.region_boss2.value,
+    Stage.region_boss3.value,
+    Stage.region_boss4.value,
+    Stage.region_boss5.value,
+    Stage.region_boss6.value,
+    Stage.region_specter1.value,
+    Stage.region_specter2.value
 ]
 
 STAGES_MASTER : Sequence[str] = [
