b402d3631f01c5f986fc4207da507762faf9abc2
aidanii <aidanii_real@proton.me>
2025-05-07 23:02:03 +0800

    - Updated version numbers

--- worlds/ape_escape_3/data/Strings.py
+++ worlds/ape_escape_3/data/Strings.py
@@ -1975,8 +1975,8 @@ class APConsole:
         greet =         " Welcome to Ape Escape 3 Archipelago!"
         game_name =     " Ape Escape 3 Archipelago"
         client_name =   " Ape Escape 3 Client"
-        client_ver =    " Client v0.4.00a R-PA"
-        world_ver =     " World v0.4.00a R-PA"
+        client_ver =    " Client v1.0.00"
+        world_ver =     " World v1.0.00"
 
         p_check =       " [|?|] Confirming PCSX2 Status..."
         p_init =        " [...] Connecting to PCSX2..."
