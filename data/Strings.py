ecbbc0c0e97c37b398298be78d7b248d08d787c0
aidanii <aidanii_real@proton.me>
2025-12-30 22:00:49 +0800

    - Updated Strings for apworld and client version

--- data/Strings.py
+++ data/Strings.py
@@ -2755,8 +2755,8 @@ class APConsole:
         greet =         "Welcome to Ape Escape 3 Archipelago!"
         game_name =     "Ape Escape 3 Archipelago"
         client_name =   "Ape Escape 3 Client"
-        client_ver =    "2.0.00-beta.5"
-        world_ver =     "2.0.00-beta.5"
+        client_ver =    "2.0.00-beta.5+1"
+        world_ver =     "2.0.00-beta.5+1"
 
         p_check =       " [|?|] Confirming PCSX2 Status..."
         p_init =        " [...] Connecting to PCSX2..."
