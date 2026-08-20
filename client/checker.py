bccf03d210018f4f08c7d37aa8306879c1e778a2
aidanii <aidanii_real@proton.me>
2026-08-20 11:51:35 +0800

    client: QSP Channel Select Preview will no longer update once a channel has been selected

--- client/checker.py
+++ client/checker.py
@@ -201,6 +201,9 @@ async def setup_level_select(ctx: "AE3Context"):
             if boss_captured:
                 ctx.ipc.unmark_location(boss)
 
+        if not is_a_level_confirmed:
+            ctx.change_current_active_channel_selection(selected_channel)
+
         # Reset Game Mode Swap state and Set Game Mode value to an unexpected value
         # as sign that the game has not yet set it
         if ctx.alt_freeplay and not is_a_level_confirmed and ctx.is_mode_swapped:
