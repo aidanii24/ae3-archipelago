9a0078024f9592d44198e810df4d80948f523950
aidanii <aidanii_real@proton.me>
2026-08-21 11:56:03 +0800

    client/widgets: Overhauled show/hide widget system to use opacity and height instead of orphaning/re-adding widgets

--- client/widgets.py
+++ client/widgets.py
@@ -2,7 +2,15 @@ from textwrap import dedent
 from typing import Any
 
 from kivy.animation import Animation, AnimationTransition
-from kivy.properties import ColorProperty, DictProperty, NumericProperty, ObjectProperty, StringProperty
+from kivy.properties import (
+    BooleanProperty,
+    ColorProperty,
+    DictProperty,
+    ListProperty,
+    NumericProperty,
+    ObjectProperty,
+    StringProperty,
+)
 from kivy.uix.widget import Widget
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
@@ -17,13 +25,12 @@ from .state import QSPDisplayMode
 
 BASE_WIDGETS: str = dedent(
     """\
-    <StatusPanel@MDBoxLayout>:
+    <StatusPanel@StatusPanelBase>:
         qsp_index_hint: 0
         qsp_modes: []
         orientation: 'vertical'
         size_hint_x: 0.98
-        size_hint_y: None
-        height: dp(40)
+        adaptive_height: True
         pos_hint: {'center_x': 0.5}
         padding: 20, 10
         md_bg_color: (*app.theme_cls.surfaceContainerHighColor[:3], 0.98)
@@ -33,37 +40,43 @@ BASE_WIDGETS: str = dedent(
         status_text: ''
         value_color: app.theme_cls.onSurfaceColor
         orientation: 'horizontal'
+        adaptive_height:True
         MDLabel:
             text: self.parent.label_text
             bold: True
             color: app.theme_cls.onSurfaceColor
-            size_hint: 0.3, 1.0
+            size_hint: 0.3, None
+            adaptive_height: True
         MDLabel:
             text: self.parent.value_text
             color: self.parent.value_color
-            size_hint: 0.4, 1.0
+            size_hint: 0.4, None
+            adaptive_height: True
             halign: 'center'
         MDLabel:
             text: self.parent.status_text
             color: app.theme_cls.onSurfaceColor
-            size_hint: 0.3, 1.0
+            size_hint: 0.3, None
+            adaptive_height: True
             halign: 'right'
     <PairedLabelComplete@PairedLabel>:
         label_text: ''
         value_text: ''
         value_color: app.theme_cls.onSurfaceColor
         size_hint_y: None
-        height: 40
+        adaptive_height:True
         orientation: 'horizontal'
         MDLabel:
             text: self.parent.label_text
             bold: True
             color: app.theme_cls.onSurfaceColor
+            adaptive_height: True
         MDLabel:
             text: self.parent.value_text
             color: app.theme_cls.onSurfaceColor
             size_hint: 0.3, 1.0
             halign: 'center'
+            adaptive_height: True
     <IndicatedStatusLabelComplete@IndicatedStatusLabel>
         label_text: ''
         value_text: ''
@@ -72,21 +85,26 @@ BASE_WIDGETS: str = dedent(
         indicator_value: 0
         indicator_color: app.theme_cls.primaryColor
         orientation: 'vertical'
+        adaptive_height: True
         MDBoxLayout:
             padding: 20, 10, 20, 5
             orientation: 'horizontal'
+            adaptive_height: True
             MDLabel:
                 text: self.parent.parent.label_text
                 color: app.theme_cls.onSurfaceColor
                 bold: True
+                adaptive_height: True
             MDLabel:
                 text: self.parent.parent.value_text
                 color: self.parent.parent.value_color
+                adaptive_height: True
                 halign: 'center'
             MDLabel:
                 text: self.parent.parent.status_text
                 color: app.theme_cls.onSurfaceColor
-                halign: 'center'
+                adaptive_height: True
+                halign: 'right'
         MDLinearProgressIndicator:
             indicator_color: self.parent.indicator_color
             size_hint_y: None
@@ -144,7 +162,6 @@ QUICK_STATUS_PANEL_KV: str = dedent(
         StatusPanel:
             id: GoalPanel
             qsp_modes: [10]
-            padding: 0
             IndicatedStatusLabelComplete:
                 id: GoalLabel
                 label_text: 'Goal Target'
@@ -154,8 +171,6 @@ QUICK_STATUS_PANEL_KV: str = dedent(
         StatusPanel:
             id: PostGameConditionPanel
             qsp_modes: [10]
-            size_hint_y: None
-            height: self.minimum_height
             padding: 20, 20, 20, 0
             spacing: 20
             MDLabel:
@@ -174,8 +189,6 @@ QUICK_STATUS_PANEL_KV: str = dedent(
                     default_size_hint: 1, None
         StatusPanel:
             id: ChannelSelectPreviewPanel
-            hint_size_y: None
-            height: self.minimum_height
             qsp_modes: [20, 30]
             padding: 20, 10, 20, 0
             spacing: 10
@@ -229,54 +242,6 @@ class QuickStatusPanel(MDBoxLayout):
         for id, widget in self.ids.items():
             self.displays[id] = widget
 
-    def _get_widget_and_cache(self, wid: str) -> Widget | None:
-        if wid not in self.displays:
-            widget: Widget = self.ids.get(wid, None)
-
-            self.displays[wid] = widget
-            return widget
-
-        return self.displays.get(wid)
-
-    def _hide_children(self, *wids):
-        """
-        Removes immediate children by id.
-        Children removed using this function are not deleted, only orphaned.
-        :param: *wids: Widget Ids of children to show
-        """
-        for wid in wids:
-            if wid in self.hidden:
-                continue
-
-            widget: Widget | None = self._get_widget_and_cache(wid)
-            if not widget:
-                continue
-
-            if widget not in self.children:
-                continue
-
-            self.remove_widget(widget)
-            self.hidden.add(wid)
-
-    def _show_children(self, *wids, indexes: list[int] | None = None):
-        """
-        Adds widgets as immediate children by id.
-        :param: *wids: Widget Ids of children to show
-        :param: index: Indexes that correspond to the index the child will be added back as if possible.
-        """
-        if not indexes:
-            indexes = []
-
-        for i, wid in enumerate(wids):
-            widget: Widget | None = self._get_widget_and_cache(wid)
-            if not widget:
-                continue
-
-            index: int = indexes[i] if i < len(indexes) else 0
-
-            self.add_widget(widget, index=index)
-            self.hidden.remove(wid)
-
     def set_display_mode(self, mode: QSPDisplayMode | int = QSPDisplayMode.MINIMAL):
         new_mode: QSPDisplayMode = QSPDisplayMode.MINIMAL
         if type(mode) is QSPDisplayMode:
@@ -290,42 +255,24 @@ class QuickStatusPanel(MDBoxLayout):
         self.display_mode = new_mode
 
     def on_display_mode(self, instance, mode):
-        to_show: list[Widget] = []
-        to_show_indexes: list[int] = []
-        to_hide: list[Widget] = []
-
-        for i, (wid, widget) in enumerate(self.displays.items()):
-            if not widget:
+        for widget in self.children:
+            if not isinstance(widget, StatusPanelBase):
                 continue
 
-            if widget.__class__.__name__ != "StatusPanel":
-                continue
-
-            if not widget.qsp_modes:
-                continue
-
-            if mode in widget.qsp_modes:
-                if wid not in self.hidden:
-                    continue
-
-                to_show.append(wid)
-                index: int = getattr(widget, "qsp_index_hint", len(self.children) + i)
-                to_show_indexes.append(index)
-            elif wid not in self.hidden:
-                to_hide.append(wid)
-
-        self._hide_children(*to_hide)
-        self._show_children(*to_show, indexes=to_show_indexes)
+            if not widget.qsp_modes or mode in widget.qsp_modes:
+                widget.is_hidden = False
+            else:
+                widget.is_hidden = True
 
     def update_game_port(self, port: int):
-        status_display: StatusLabel | None = self._get_widget_and_cache("StatusLabel")
+        status_display: StatusLabel | None = self.ids.get("StatusLabel", None)
         if not status_display:
             return
 
         status_display.set_status_text(f"Port: {port}")
 
     def update_game_status(self, status: ConnectionStatus):
-        status_display: StatusLabel | None = self._get_widget_and_cache("StatusLabel")
+        status_display: StatusLabel | None = self.ids.get("StatusLabel", None)
         if not status_display:
             return
 
@@ -340,7 +287,7 @@ class QuickStatusPanel(MDBoxLayout):
                 status_display.set_value_text("In Game", self.theme_cls.primaryColor)
 
     def set_goal_target_status(self, goal_target: str, current_amount: int = 0, target_amount: int = 0):
-        goal_display: StatusLabel | None = self._get_widget_and_cache("GoalLabel")
+        goal_display: StatusLabel | None = self.ids.get("GoalLabel", None)
         if not goal_display:
             return
 
@@ -348,21 +295,21 @@ class QuickStatusPanel(MDBoxLayout):
         goal_display.set_value_text(goal_target)
 
     def update_goal_target_status(self, current_amount: int, target_amount: int):
-        goal_display: StatusLabel | None = self._get_widget_and_cache("GoalLabel")
+        goal_display: StatusLabel | None = self.ids.get("GoalLabel", None)
         if not goal_display:
             return
 
         goal_display.set_status_text(f"{current_amount}/{target_amount}")
 
     def update_pgc_status(self, data: list[dict]):
-        pgc_view: AE3RecycleView | None = self._get_widget_and_cache("PostGameConditionView")
+        pgc_view: AE3RecycleView | None = self.ids.get("PostGameConditionView", None)
         if not pgc_view:
             return
 
         pgc_view.set_data(data)
 
     def set_channel_select_preview_labels(self, channel_names: list[str]):
-        cspl: ChannelSelectPreviewLayout | None = self._get_widget_and_cache("ChannelSelectPreviewLayout")
+        cspl: ChannelSelectPreviewLayout | None = self.ids.get("ChannelSelectPreviewLayout", None)
         if not cspl:
             return
 
@@ -373,11 +320,11 @@ class QuickStatusPanel(MDBoxLayout):
         if not data:
             data = []
 
-        csp: AE3ScrollView | None = self._get_widget_and_cache("ChannelSelectPreviewScroll")
+        csp: AE3ScrollView | None = self.ids.get("ChannelSelectPreviewScroll", None)
         if not csp or csp in self.hidden:
             return
 
-        cspl: ChannelSelectPreviewLayout | None = self._get_widget_and_cache("ChannelSelectPreviewLayout")
+        cspl: ChannelSelectPreviewLayout | None = self.ids.get("ChannelSelectPreviewLayout", None)
         if not cspl:
             return
 
@@ -390,7 +337,7 @@ class QuickStatusPanel(MDBoxLayout):
         csp.scroll_to_x(scroll_ratio)
 
     def update_overview_status(self, data: list[dict], total: dict | None = None):
-        overview_view: AE3RecycleView | None = self._get_widget_and_cache("OverviewView")
+        overview_view: AE3RecycleView | None = self.ids.get("OverviewView", None)
         if not overview_view:
             return
 
@@ -398,13 +345,22 @@ class QuickStatusPanel(MDBoxLayout):
         if not total:
             return
 
-        cspp: IndicatedPairedLabel = self._get_widget_and_cache("ChannelSelectPreviewProgress")
+        cspp: IndicatedPairedLabel = self.ids.get("ChannelSelectPreviewProgress", None)
         if not cspp:
             return
 
         cspp.set_label_text(total.get("label_text", ""))
         cspp.set_value_text(total.get("value_text", ""))
         cspp.set_indicator_value(total.get("indicator_value", 50))
+
+
+class StatusPanelBase(MDBoxLayout):
+    qsp_modes: ListProperty
+    is_hidden = BooleanProperty(False)
+
+    def on_is_hidden(self, instance, is_hidden):
+        instance.height = 0 if is_hidden else instance.minimum_height
+        instance.opacity = 0 if is_hidden else 1.0
 
 
 class AE3RecycleView(MDRecycleView):
