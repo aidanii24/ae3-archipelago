d98d8ee2b55ff7d29185534faf3fb7a41ec3676a
aidanii <aidanii_real@proton.me>
2026-08-28 18:54:09 +0800

    manager: Fixed Overview Data for Bosses showing data for other bosses off by an index of 1

--- client/manager.py
+++ client/manager.py
@@ -1227,6 +1227,7 @@ class AE3Context(SuperContext):
         else:
             channel_id = [*CHANNEL_ID_TO_NAME.keys()][[*CHANNEL_ID_TO_NAME.values()].index(channel_name)]
 
+        print(channel_id, channel_name)
         if channel_id in Locations.MONKEYS_DIRECTORY:
             monkeys: set[str] = set(Locations.MONKEYS_DIRECTORY.get(channel_id, []))
             monkeys.difference_update(Locations.MONKEYS_PASSWORDS)
@@ -1246,7 +1247,7 @@ class AE3Context(SuperContext):
             boss_index: int = int(channel_id[-1])
 
             if 0 < boss_index < len(Locations.MONKEYS_BOSSES):
-                target: int = self.locations_name_to_id.get(Locations.MONKEYS_BOSSES[boss_index], 0)
+                target: int = self.locations_name_to_id.get(Locations.MONKEYS_BOSSES[boss_index - 1], 0)
                 is_cleared: bool = target in self.locations_checked
                 data.append(
                     {
